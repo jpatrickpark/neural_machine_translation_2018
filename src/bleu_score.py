828fe171871a7faf24dd83f3be91efada0072278
wywang-nyu <ayw255@nyu.edu>
2018-11-07 13:35:59 -0500

    change file destination

---
+++ src/bleu_score.py
@@ -0,0 +1,17 @@
+import sacrebleu
+from detok import detok
+import numpy as np
+
+def bleu(trg.vocab.itos, translation_output, reference):
+    '''
+    Args:
+        trg.vocab.itos: a list the match indices to string.
+        translation_output: 2D tensor of tranlation output. shape: N x B
+        reference: 1D list of reference sentences (words, not indices). len(reference) = B
+    '''
+    EN_ind2word = np.array(trg.vocab.itos)
+    detok_translation = detok.detok(translation_output, EN_ind2word)
+    bleu_score = sacrebleu.raw_corpus_bleu(detok_translation, [reference], .01).score
+    
+
+    return bleu_score
